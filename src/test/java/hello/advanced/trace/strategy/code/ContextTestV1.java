efcdf09786706b14e8f74a083be5682c9f2fd813
nara113 <0554my@naver.com>
2022-02-28 04:05:27 +0900

    전략 패턴 - 예제 2

--- src/test/java/hello/advanced/trace/strategy/code/ContextTestV1.java
+++ src/test/java/hello/advanced/trace/strategy/code/ContextTestV1.java
@@ -4,8 +4,10 @@ import hello.advanced.trace.strategy.code.strategy.ContextV1;
 import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class ContextTestV1 {
     @Test
     void strategyV1() {
@@ -15,6 +17,36 @@ class ContextTestV1 {
 
         Strategy strategy2 = new StrategyLogic2();
         ContextV1 context2 = new ContextV1(strategy2);
+        context2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+        Strategy strategy1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("logic 1");
+            }
+        };
+        ContextV1 context1 = new ContextV1(strategy1);
+        context1.execute();
+
+        Strategy strategy2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("logic 2");
+            }
+        };
+        ContextV1 context2 = new ContextV1(strategy2);
+        context2.execute();
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 context1 = new ContextV1(() -> log.info("logic 1"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("logic 2"));
         context2.execute();
     }
 }
